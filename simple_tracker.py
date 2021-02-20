27c2a8f336afe6ec5e2ae5f23ae1364bda3c9e49
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-02-20 11:25:53 +0100

    comments on all major lines of code

--- simple_tracker.py
+++ simple_tracker.py
@@ -1,6 +1,8 @@
+# Imports from the amazon_config file
 from amazon_config import(get_web_driver_options, get_chrome_web_driver, set_ignore_certificate_error,
                           set_browser_as_incognito, NAME, CURRENCY, FILTERS, BASE_URL, DIRECTORY)
 
+# Imports from third party libraries
 from selenium.webdriver.common.keys import Keys
 import time
 
@@ -9,39 +11,37 @@ class GenerateReport:
     def __init__(self):
         pass
 
+# Creating a class that scrapes all needed information from the Base URL
+
 
 class AmazonAPI:
+    # Initialising all values needed
     def __init__(self, search_term, filters, base_url, currency):
-        self.base_url = base_url
         self.search_term = search_term
+        self.filters = f"&rh=p_36%3A{filters['min']}00-{filters['max']}00"
+        self.base_url = base_url
+        self.currency = currency
         options = get_web_driver_options()
+        self.driver = get_chrome_web_driver(options)
         set_ignore_certificate_error(options)
         set_browser_as_incognito(options)
-        self.driver = get_chrome_web_driver(options)
-        self.currency = currency
-        self.filters = f"&rh=p_36%3A{filters['min']}00-{filters['max']}00"
-
-    def run(self):
-        print('Starting script...')
-        print(f"Looking for {self.search_term} products...")
-        links = self.get_products_links()
-        time.sleep(1)
-        if not links:
-            print('Stopped script.')
-            return
-        print(f"Got {len(links)} links to products...")
-        print("Getting info about products")
-        self.driver.quit()
 
     def get_products_links(self):
+        # Open the browser and go to Base URL
         self.driver.get(self.base_url)
-        element = self.driver.find_element_by_xpath('//*[@id="twotabsearchtextbox"]')
+        # Find searchbar
+        element = self.driver.find_element_by_xpath(
+            '//*[@id="twotabsearchtextbox"]')
+        # Type in search term
         element.send_keys(self.search_term)
+        # Start search
         element.send_keys(Keys.ENTER)
         time.sleep(2)
+        # Set filters
         self.driver.get(f'{self.driver.current_url}{self.filters}')
         print(f"Our url: {self.driver.current_url}")
         time.sleep(2)
+        # Create a list of all results found on base URL fitting our filters
         result_list = self.driver.find_elements_by_class_name('s-result-list')
         links = []
         try:
@@ -53,6 +53,75 @@ class AmazonAPI:
             print("Didn't get any products...")
             print(e)
             return links
+
+    # Function to cut out the product Id out of link
+    def get_asin(self, product_link):
+        return product_link[product_link.find('/dp/') + 4:product_link.find('/ref')]
+
+    # Using function that cuts out product Id on all returned link
+    def get_asins(self, links):
+        return [self.get_asin(link) for link in links]
+
+    # Function to create the essential link needed to find product
+    def shorten_url(self, asin):
+        return self.base_url + 'dp/' + asin
+
+    # Getting the title of the product from every link returned
+    def get_title(self):
+        try:
+            return self.driver.find_elements_by_id('productTitle').text
+        except Exception as e:
+            print(e)
+            print(f"Can't get title of a product - {self.driver.current_url}")
+            return None
+
+    # Getting the seller of the product from every link returned
+    def get_seller(self):
+        try:
+            return self.driver.find_elements_by_id('bylineInfo').text
+        except Exception as e:
+            print(e)
+            print(f"Can't get seller of a product - {self.driver.current_url}")
+            return None
+
+    # Getting the price of the product from every link returned
+    def get_price(self):
+        return '99€'
+
+    # Function that rund the functions to get information for single product
+    def get_single_product_info(self, asin):
+        print(f"Product ID: {asin} - getting data...")
+        product_short_url = self.shorten_url(asin)
+        self.driver.get(f'{product_short_url}?language=en_GB')
+        time.sleep(2)
+        title = self.get_title()
+        seller = self.get_seller()
+        price = self.get_price()
+
+    # Calling the function that gets information about the product on all returned links
+    def get_products_info(self, links):
+        asins = self.get_asins(links)
+        products = []
+        for asin in asins:
+            product = self.get_single_product_info(asin)
+
+    # Function that first calls the function to get all relevant links and then get the information from these links
+    def run(self):
+        print('Starting script...')
+        print(f"Looking for {self.search_term} products...")
+        # Get all relevant links
+        links = self.get_products_links()
+        time.sleep(1)
+        # Default if no links can be found
+        if not links:
+            print('Stopped script.')
+            return
+        # Printing the information how many links found
+        print(f"Got {len(links)} links to products...")
+        print("Getting info about products...")
+        # Getting information from all links
+        products = self.get_products_info(links)
+        self.driver.quit()
 
 
 if __name__ == '__main__':
